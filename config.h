d7fa4495da379e841520bcb53c3176d9d77e0455
PluemW <96386038+PluemW@users.noreply.github.com>
2024-01-26 00:11:44 +0700

    Add files via upload

---
+++ config.h
@@ -0,0 +1,16 @@
+#ifndef config
+#define config
+
+#define BMotor_speed 30000
+#define MMotor_speed 20000
+#define SMotor_speed 12000
+
+#define Baccel 40000
+#define Maccel 30000
+#define Saccel 16000
+
+#define yzero -500000
+#define zset 30000
+#define zzero 500000
+
+#endif
---
+++ pin.h
@@ -0,0 +1,26 @@
+#ifndef pin
+#define pin
+
+// 27,5,21,33,35
+ 
+// Sensor
+#define lim1 32   //y1
+#define lim2 5    //y2
+#define lim3 26   //socket1
+//#define lim4 33    //socket2
+#define lim5 2    //socket3
+
+// Motor/controllor
+#define lock 4    //magnet
+#define stdir1 18 //y1
+#define stpwm1 19 //y1
+#define stdir2 14 //y2
+#define stpwm2 15 //y2
+#define stdir3 12 //socket1
+#define stpwm3 21 //socket1
+#define stdir4 16 //socket2
+#define stpwm4 17 //socket2
+#define stdir5 23 //socket3
+#define stpwm5 22 //socket3
+
+#endif
